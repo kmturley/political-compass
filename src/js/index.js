d32215dbec114f63d3ece8f764fdb9b092a0be4f
Kim T <kimturley@gmail.com>
2020-01-03 17:06:03 -0800

    Draw chart using javascript

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+(function() {
+  var canvas = document.getElementById('chart');
+  var context = canvas.getContext('2d');
+
+  function setupChart() {
+    var height = canvas.height = canvas.parentNode.clientWidth; // use width for square aspect ratio
+    var width = canvas.width = canvas.parentNode.clientWidth;
+    console.log('setupChart', width, height);
+    context.clearRect(0, 0, width, height);
+    drawLine(
+      { x: 0, y: height / 2},
+      { x: width, y: height / 2}
+    );
+    drawLine(
+      { x: width / 2, y: 0},
+      { x: width / 2, y: height}
+    );
+  }
+
+  function drawLine(start, end) {
+    context.moveTo(start.x, start.y);
+    context.lineTo(end.x, end.y);
+    context.stroke();
+  }
+
+  window.addEventListener('resize', function() {
+    setupChart();
+  });
+
+  setupChart();
+}());
